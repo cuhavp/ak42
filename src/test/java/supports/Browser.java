44ced9cb0dea9b5cc910e51aa75fc1e23fff6659
hadothanh <ha.do@kyber.network>
2023-12-02 14:51:04 +0700

    change to chrome headless mode

--- src/test/java/supports/Browser.java
+++ src/test/java/supports/Browser.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -25,7 +26,9 @@ public class Browser {
 
     public static void open(String browser){
         if (browser.equalsIgnoreCase("chrome")) {
-            driver= new ChromeDriver();
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless=new");
+            driver= new ChromeDriver(chromeOptions);
         } else if (browser.equalsIgnoreCase("firefox")) {
             driver =new FirefoxDriver();
         } else if (browser.equalsIgnoreCase("edge")) {
